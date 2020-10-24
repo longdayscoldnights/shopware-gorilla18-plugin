161a270ca59df2521506025f358e14b63af69d85
Joschi <joschi_mehta@gmx.de>
2020-10-24 21:08:37 +0200

    adding user authentification

--- Controllers/Frontend/ControllerDemo.php
+++ Controllers/Frontend/ControllerDemo.php
@@ -4,6 +4,15 @@ class Shopware_Controllers_Frontend_ControllerDemo extends Enlight_Controller_Ac
 {
     public function preDispatch() 
     {
+        // This is for user authentification, will use this code later for adults non adults to redirect to different pages
+        if($this->Request()->getActionName() === 'index' && !$this->get('session')->get('sUserId')) {
+            $this->redirect([
+                'controller' => 'account',
+                'action' => 'login',
+                'sTarget' => 'ControllerDemo',
+                'sTargetAction' => $this->Request()->getActionName()
+            ]);
+        }
         $this->view->addTemplateDir(__DIR__ . '/../../Resources/views');
     }
     public function indexAction()
